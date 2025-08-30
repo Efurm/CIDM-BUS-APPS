f953c815059eafd81ed595d166d068de1fc47bfb
Efurm <efurman1@buffs.wtamu.edu>
2025-08-29 21:53:00 -0500

    Submit Homework1

---
+++ Homework1.cs
@@ -0,0 +1,12 @@
+﻿namespace Homework1;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        var X = 2.5;
+        var Y = 3.3;
+        var Z = 4*X*X+3*Y;
+        Console.WriteLine($"The Value of Z is: {Z}");
+    }
+}
